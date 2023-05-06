999e7b0dd1c6db1b98bfdb95fa6f8fd49b7f7577
vezaadiloviana <vezaadiloviana27@gmail.com>
2023-05-06 13:21:42 +0700

    modal detail produk

--- resources/views/livewire/table/daftar-produk.blade.php
+++ resources/views/livewire/table/daftar-produk.blade.php
@@ -72,7 +72,7 @@
                         <div id="dropdownDetailToko" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
                             <ul class="py-2 text-sm text-gray-600 dark:text-gray-200" aria-labelledby="dropdownMenuDetailToko">
                                 <li>
-                                    <button type="button" class="flex gap-2 items-center w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <button data-modal-toggle="detail-produk" type="button" class="flex gap-2 items-center w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                                         <span class="iconify w-5 h-5" data-icon="solar:document-text-bold-duotone"></span>
                                         Detail
                                     </button>
--- resources/views/role/admin/PageDashboardAdmin.blade.php
+++ resources/views/role/admin/PageDashboardAdmin.blade.php
@@ -28,6 +28,7 @@
                 <livewire:table.daftar-produk/>
                 <livewire:modal.tambah-produk/>
                 <livewire:modal.import-produk/>
+                <livewire:modal.detail-produk/>
                 <livewire:modal.edit-produk/>
                 <livewire:modal.hapus-produk/>
             </div>
